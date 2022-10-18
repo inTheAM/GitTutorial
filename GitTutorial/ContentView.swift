c78dcea97da6402da8e0e8393788d7e846e04cff
Ahmed Mgua <mguaahmed@gmail.com>
2022-10-18 15:34:33 +0200

    Initial Commit

---
+++ GitTutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitTutorial
+//
+//  Created by Ahmed Mgua on 18/10/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GitTutorial/GitTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitTutorialApp.swift
+//  GitTutorial
+//
+//  Created by Ahmed Mgua on 18/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct GitTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
