dd925b0855c0d80520643ff7cd20a70e9989e38d
Ahmed Mgua <mguaahmed@gmail.com>
2022-10-18 15:59:13 +0200

    Added View1 ......

---
+++ GitTutorial/View1.swift
@@ -0,0 +1,25 @@
+//
+//  View1.swift
+//  GitTutorial
+//
+//  Created by Ahmed Mgua on 18/10/2022.
+//
+
+import SwiftUI
+
+struct View1: View {
+    
+    var body: some View {
+        VStack {
+            Text("Hello")
+            Image(systemName: "flame.fill")
+            
+        }
+    }
+}
+
+struct View1_Previews: PreviewProvider {
+    static var previews: some View {
+        View1()
+    }
+}
